7ddda38ae3cbc2cac5a9a6676e0881ebe6144421
Andrey <65368541+GorizontX@users.noreply.github.com>
2023-03-10 17:47:34 +0100

    1. Set up Alert Controllers

--- LogInApp/ViewController.swift
+++ LogInApp/ViewController.swift
@@ -14,6 +14,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func forgotNameTapped(_ sender: Any) {
+        let alertController = UIAlertController(title: "It's so cute 🥰", message: "You forgot your name. Check your passport 😉", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        
+        alertController.addAction(action)
+        self.present(alertController, animated: true)
+        
+    }
+    
+    @IBAction func forgotPasswordTapped(_ sender: Any) {
+        let alertController = UIAlertController(title: "That is not cute 😐", message: "Your password is 13579, please remember it.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        
+        alertController.addAction(action)
+        self.present(alertController, animated: true)
+        
+    }
+    
+    
+    
 }
 
