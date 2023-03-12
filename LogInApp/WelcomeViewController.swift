5b4d066f18462c75502adcf3990c1e6cb293a65d
Andrey <65368541+GorizontX@users.noreply.github.com>
2023-03-12 01:02:39 +0100

    1. Placed the elements on the WelcomeViewController

---
+++ LogInApp/WelcomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeViewController.swift
+//  LogInApp
+//
+//  Created by Andrey Machulin on 11.03.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func logoutTapped() {
+        dismiss(animated: true)
+    }
+    
+}
